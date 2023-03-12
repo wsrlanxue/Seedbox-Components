7256d3b3c8d2b427861481bd5441716db24a2235
wsrlanxue <127671603+wsrlanxue@users.noreply.github.com>
2023-03-12 21:55:56 +0800

    Update seedbox_installation.sh

--- seedbox_installation.sh
+++ seedbox_installation.sh
@@ -13,7 +13,7 @@ function Update {
 ## qBittorrent
 function qBittorrent {
     warn_2
-    source <(wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qBittorrent_install.sh)
+    source <(wget -qO- https://raw.githubusercontent.com/wsrlanxue/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qBittorrent_install.sh)
     qBittorrent_download
     qBittorrent_install
     qBittorrent_config
@@ -24,7 +24,7 @@ function qBittorrent {
 ## Deluge
 function Deluge {
     warn_2
-    source <(wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Torrent%20Clients/Deluge/Deluge_install.sh)
+    source <(wget -qO- https://raw.githubusercontent.com/wsrlanxue/Seedbox-Components/main/Torrent%20Clients/Deluge/Deluge_install.sh)
     Deluge_download
     Deluge_install
     Deluge_config
