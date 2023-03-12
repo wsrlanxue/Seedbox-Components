fabdce084c48f9ac161353fed80cdd1aa0d0930c
wsrlanxue <127671603+wsrlanxue@users.noreply.github.com>
2023-03-12 21:59:35 +0800

    Update qBittorrent_install.sh

--- Torrent Clients/qBittorrent/qBittorrent_install.sh
+++ Torrent Clients/qBittorrent/qBittorrent_install.sh
@@ -32,7 +32,7 @@ function qBittorrent_download {
             *) warn_1; echo "Please choose a valid version"; normal_3;;
         esac
     done
-    wget https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qBittorrent/qBittorrent%20$qBver%20-%20$libver/qbittorrent-nox && chmod +x $HOME/qbittorrent-nox
+    wget https://raw.githubusercontent.com/wsrlanxue/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qBittorrent/qBittorrent%20$qBver%20-%20$libver/qbittorrent-nox && chmod +x $HOME/qbittorrent-nox
 }
 
 function qBittorrent_install {
@@ -111,8 +111,8 @@ WebUI\Port=8080
 WebUI\Username=$username
 EOF
     elif [[ "${qBver}" =~ "4.2."|"4.3." ]]; then
-        wget  https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qb_password_gen && chmod +x $HOME/qb_password_gen
-        PBKDF2password=$($HOME/qb_password_gen $password)
+#        wget  https://raw.githubusercontent.com/wsrlanxue/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qb_password_gen && chmod +x $HOME/qb_password_gen
+#        PBKDF2password=$($HOME/qb_password_gen $password)
         cat << EOF >/home/$username/.config/qBittorrent/qBittorrent.conf
 [BitTorrent]
 Session\AsyncIOThreadsCount=$aio
@@ -131,12 +131,12 @@ Connection\PortRangeMin=45000
 Downloads\DiskWriteCacheSize=$Cache_qB
 Downloads\SavePath=/home/$username/qbittorrent/Downloads/
 Queueing\QueueingEnabled=false
-WebUI\Password_PBKDF2="@ByteArray($PBKDF2password)"
+WebUI\Password_PBKDF2="@ByteArray(Not1vVhJQdDR+kN1qTEovg==:BCa2IB/LV0Ix1ndUQEcVz4xHBCgiIJ0B8DpmN9OG3E2yJUPwECQHjvigHmM6oKUARTx/kahbXVL7sn/Ryyi8jg==)"
 WebUI\Port=8080
 WebUI\Username=$username
 EOF
     elif [[ "${qBver}" =~ "4.4."|"4.5." ]]; then
-        wget  https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qb_password_gen && chmod +x $HOME/qb_password_gen
+        wget  https://raw.githubusercontent.com/wsrlanxue/Seedbox-Components/main/Torrent%20Clients/qBittorrent/qb_password_gen && chmod +x $HOME/qb_password_gen
         PBKDF2password=$($HOME/qb_password_gen $password)
         cat << EOF >/home/$username/.config/qBittorrent/qBittorrent.conf
 [Application]
